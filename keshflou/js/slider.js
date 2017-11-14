a0134d8f14e21d2b243e8ee1b2285b41e55b2ae8
safronman <safronmanbox@gmail.com>
2017-11-14 16:52:24 +0300

    add slider photo

---
+++ keshflou/js/slider.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+$('.partner__slider').slick({
+  arrows: false,
+  dots: true,
+  variableWidth: true
+});
+
+$('.review__slider').slick({
+  arrows: false,
+  dots: true,
+  slidesToShow: 4
+});
+
+$('.photo__slider').slick({
+   arrows: false,
+   dots: true,
+   slidesToShow: 3
+ });
+
+});
