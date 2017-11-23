6ed90ce6c550afe175b6fa54d7731e9e221d2128
safronman <safronmanbox@gmail.com>
2017-11-23 10:10:51 +0300

    добавил всплывающую форму

--- keshflou/javascript/script.js
+++ keshflou/javascript/script.js
@@ -1,3 +1,4 @@
+//slick sliders
 $(document).ready(function(){
 
 $('.partner__slider').slick({
@@ -18,4 +19,35 @@ $('.photo__slider').slick({
    slidesToShow: 3
  });
 
+});
+
+
+//modal form
+
+//открыть модальное окно
+var formOpen = document.querySelector(".btn--header");
+//закрыть модально окно
+var formClose = document.querySelector(".modal__close-form");
+//модально окно
+var modalWindow = document.querySelector(".modal__form");
+//модальное окно наложения
+var overlayWindow = document.querySelector(".modal__overlay");
+
+//открытие модальной формы по клику на копку Заказать звонок
+formOpen.addEventListener("click", function(){
+  event.preventDefault();
+  modalWindow.classList.add("modal__form--show");
+  overlayWindow.classList.add("modal__overlay--show");
+});
+//закрытие модальной формы по клику на крестик
+formClose.addEventListener("click", function(){
+  event.preventDefault();
+  modalWindow.classList.remove("modal__form--show");
+  overlayWindow.classList.remove("modal__overlay--show");
+});
+//закрытие модальной формы по клику на слой наложения
+overlayWindow.addEventListener("click", function(){
+  event.preventDefault();
+  modalWindow.classList.remove("modal__form--show");
+  overlayWindow.classList.remove("modal__overlay--show");
 });
