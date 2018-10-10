8258d0f3f024e82d72902430d430ec183916dbbe
safronman <safronmanbox@gmail.com>
2018-10-10 14:25:27 +0300

    uncoment closure file

--- js_projects/6_closures/js/main.js
+++ js_projects/6_closures/js/main.js
@@ -1,52 +1,52 @@
-// // 1. Замыкания, это функция объявленная внутри функции, благодаря которой локальные переменные внутри старшей функции не сбрасываются после завершения работы этой функции, а остаются при повторном вызове.
-// // Замыканиями они называются потому, что младшая функция их замыкает внутри старшей.
+// 1. Замыкания, это функция объявленная внутри функции, благодаря которой локальные переменные внутри старшей функции не сбрасываются после завершения работы этой функции, а остаются при повторном вызове.
+// Замыканиями они называются потому, что младшая функция их замыкает внутри старшей.
 
 
-// // 2. Замыкание возникает в том случае когда мы из одной функции makeCounter() возвращаем новую функцию counter.
-// // Тогда мы говорим, что новая функция counter() замкнута на область видимости родительской функции makeCounter()
+// 2. Замыкание возникает в том случае когда мы из одной функции makeCounter() возвращаем новую функцию counter.
+// Тогда мы говорим, что новая функция counter() замкнута на область видимости родительской функции makeCounter()
 
-// function makeCounter() {
-//   let currentCount = 0;
+function makeCounter() {
+  let currentCount = 0;
 
-//   // область видимости функции counter замкнута на переменную currentCount
-//   function counter() {
-//     currentCount++;
-//     return currentCount;
-//   }
+  // область видимости функции counter замкнута на переменную currentCount
+  function counter() {
+    currentCount++;
+    return currentCount;
+  }
 
-//   return counter;
-// };
-
-
-// // вывод данных в консоль
-// var counter1 = makeCounter();
-// var counter2 = makeCounter();
-
-// console.log(counter1());
-// console.log(counter1());
-// console.log(counter1());
-
-// console.log(counter2());
-// console.log(counter2());
-// console.log(counter2());
+  return counter;
+};
 
 
-// // вывод данных путем добаления события клик на кнопки
-// var counter3 = makeCounter();
-// var counter4 = makeCounter();
+// вывод данных в консоль
+var counter1 = makeCounter();
+var counter2 = makeCounter();
 
-// var button1 = document.querySelector('.button1');
-// var button2 = document.querySelector('.button2');
-// var value1 = document.querySelector('.value1');
-// var value2 = document.querySelector('.value2');
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
 
-// button1.addEventListener('click', function () {
-//   value1.innerHTML = counter3();
-// });
+console.log(counter2());
+console.log(counter2());
+console.log(counter2());
 
-// button2.addEventListener('click', function () {
-//   value2.innerHTML = counter4();
-// });
+
+// вывод данных путем добаления события клик на кнопки
+var counter3 = makeCounter();
+var counter4 = makeCounter();
+
+var button1 = document.querySelector('.button1');
+var button2 = document.querySelector('.button2');
+var value1 = document.querySelector('.value1');
+var value2 = document.querySelector('.value2');
+
+button1.addEventListener('click', function () {
+  value1.innerHTML = counter3();
+});
+
+button2.addEventListener('click', function () {
+  value2.innerHTML = counter4();
+});
 
 
 // 2 пример
