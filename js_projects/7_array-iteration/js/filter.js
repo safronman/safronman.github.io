90ce2d82fa3d645eaca9ec8b4ea1c117c52c43a7
safronman <safronmanbox@gmail.com>
2018-10-16 11:32:56 +0300

    add new examples, correct description, add screenshots

--- js_projects/7_array-iteration/js/filter.js
+++ js_projects/7_array-iteration/js/filter.js
@@ -1,9 +1,10 @@
 // 1. Filter
+// Filter возвращает НОВЫЙ массив.
 // Назначение Filter:
-// пробежаться по всему массиву и выбрать те элементы,
-// которые подпадают под назначенный нами критерий  
+// 1) для фильтрации массива
+// 2) пробежаться по всему массиву и выбрать те элементы, которые подпадают под назначенный нами критерий  
 
-// Задача:
+// Задача 1:
 // Перебрать массив чисел и вывести все отрицательные числа в другой массив
 
 let numberArray = [1, -4, 0, 23, -98, -87, 55];
@@ -25,4 +26,47 @@ let resultArray2 = numberArray.filter((item) => {
 console.log(numberArray);
 console.log(resultArray1);
 console.log(resultArray2);
+
+
+
+// // Задача 2:
+// // Вывести в результирующий массив не пустые строки
+
+// let array = ['Minsk', 'Moscow', '', '', 'London', ''];
+
+// let resultArray = array.filter(function(item) {
+//   return item !== '';
+// })
+
+// console.log(array);
+// console.log(resultArray);
+
+
+
+// // Задача 3:
+// // Пропускаем на вечеринку только тех, кто девочка и кому 18+
+
+// let array = [{
+//     age: 18,
+//     sex: 'f',
+//     name: 'Sveta'
+//   },
+//   {
+//     age: 17,
+//     sex: 'f',
+//     name: 'Sashka'
+//   },
+//   {
+//     age: 19,
+//     sex: 'm',
+//     name: 'Andrew'
+//   }
+// ]
+
+// let resultArray = array.filter(function(item) {
+//   return item.age >= 18 && item.sex === 'f'; 
+// })
+
+// console.log(array);
+// console.log(resultArray);
 
--- js_projects/7_array-iteration/js/reduce.js
+++ js_projects/7_array-iteration/js/reduce.js
@@ -1,8 +1,11 @@
 // 5. Reduce
-// Метод Reduce производит пошагавую аккумуляцию элементов массива.
-// Метод Reduce проходит по массиву и обращаем внимание не только на текущий жлемент массива, но и на то, что было подсчитано до него
+// Назначение Reduce:
+// 1) служит для для прохода по массиву с вычислением значения
+// 2) метод Reduce пробегается по всему массиву и на выход выдаёт какое-то одно обобщённое значение. Это может быть как новый массив, так и простое значение примитив или объект...
+// 3) Метод Reduce производит пошагавую аккумуляцию элементов массива.
+// Метод Reduce проходит по массиву и обращает внимание не только на текущий элемент массива, но и на то, что было подсчитано до него
 
-// Задача:
+// Задача 1 :
 // Подсчитать сумму элементов массива
 
 let numberArray = [1, -4, 0, 23, -98, -87, 55];
@@ -18,8 +21,59 @@ for (let i = 0; i < numberArray.length; i++) {
 // Метод reduce
 // В роли аккумулятора, накпливающего знчения выступает параметр total
 let sum2 = numberArray.reduce((total, item) => {
-  return total + item;
+  // return total + item;
+  total = total + item;
+  return total;
+
 }, 0);
 
 console.log(sum);
 console.log(sum2);
+
+
+// // Задача 2 :
+// // Подсчитать количество не пустых строк в массиве 
+
+// let array = ['Minsk', 'Moscow', '', '', 'London', ''];
+
+// let resultArray = array.reduce(function(total, item) {
+//   if (item !== '') {
+//     total++;
+//   }
+//   return total;
+// }, 0);
+
+
+// console.log(array);
+// console.log(resultArray);
+
+
+
+// // Задача 3 :
+// // на выходе получаем новый массив, состоящий из людей, кто девочка и кому 18+ (но лучше эту задачу решать с помощью filter, но и так можно)
+
+// let array = [{
+//   age: 18,
+//   sex: 'f',
+//   name: 'Sveta'
+// }, {
+//   age: 17,
+//   sex: 'f',
+//   name: 'Sashka'
+// }, {
+//   age: 19,
+//   sex: 'm',
+//   name: 'Andrew'
+// }];
+
+// let resultArray = array.reduce(function(total, item) {
+//   if (item.age >= 18 && item.sex === 'f') {
+//     total.push(item)
+//   } 
+//   return total;
+// }, []);
+
+
+// console.log(array);
+// console.log(resultArray);
+
