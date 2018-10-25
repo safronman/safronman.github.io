a5871ef3797fe1d573681c5d587246348853e95b
safronman <safronmanbox@gmail.com>
2018-10-25 13:30:07 +0300

    add filter.js

---
+++ reveb/js/filter.js
@@ -0,0 +1,11 @@
+let startPoint = document.querySelector('.min-toggle');
+let endPoint = document.querySelector('.max-toggle');
+let startInput = document.querySelector('.min-price');
+let endInput = document.querySelector('.max-price');
+
+
+// startInput.addEventListener('input', setPriceFrom);
+
+// function setPriceFrom(event) {
+//   startInput.value = 10;
+// }
