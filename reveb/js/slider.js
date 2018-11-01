2542f7062300f24f24ccecf956efe94c2ed0a9e9
safronman <safronmanbox@gmail.com>
2018-11-01 16:14:35 +0300

    wrap in self-invoking functions

--- reveb/js/slider.js
+++ reveb/js/slider.js
@@ -1,24 +1,25 @@
-let slides = document.querySelectorAll('.slide');
-let sliderControls = document.querySelectorAll('.slider-control');
-// переводим nodelist in array
-let arraySliderControls = [... sliderControls];
+(function () {
+  let slides = document.querySelectorAll('.slide');
+  let sliderControls = document.querySelectorAll('.slider-control');
+  // переводим nodelist in array
+  let arraySliderControls = [...sliderControls];
 
-for (let i = 0; i < sliderControls.length; i++) {
-  sliderControls[i].addEventListener('click', makeActiveControl);
-  sliderControls[i].addEventListener('click', showSlide);
-}
+  for (let i = 0; i < sliderControls.length; i++) {
+    sliderControls[i].addEventListener('click', makeActiveControl);
+    sliderControls[i].addEventListener('click', showSlide);
+  }
 
-function makeActiveControl(event) {
-  let activeControl = document.querySelector('.slider-control-active');
-  activeControl.classList.remove('slider-control-active');
-  event.currentTarget.classList.add('slider-control-active');
-}
+  function makeActiveControl(event) {
+    let activeControl = document.querySelector('.slider-control-active');
+    activeControl.classList.remove('slider-control-active');
+    event.currentTarget.classList.add('slider-control-active');
+  }
 
-function showSlide(event) {
-  let activeSlide = document.querySelector('.slide-show');
-  activeSlide.classList.remove('slide-show');
-  let indexEl = arraySliderControls.indexOf(event.currentTarget);
-  slides[indexEl].classList.add('slide-animation');
-  slides[indexEl].classList.add('slide-show');
-}
-
+  function showSlide(event) {
+    let activeSlide = document.querySelector('.slide-show');
+    activeSlide.classList.remove('slide-show');
+    let indexEl = arraySliderControls.indexOf(event.currentTarget);
+    slides[indexEl].classList.add('slide-animation');
+    slides[indexEl].classList.add('slide-show');
+  }
+})();
