95c9e112982c077e54d5d548a089c607767421f4
safronman <safronmanbox@gmail.com>
2018-11-01 16:16:01 +0300

    beautify template array

--- reveb/js/templateArray.js
+++ reveb/js/templateArray.js
@@ -6,50 +6,45 @@ let templateArray = [
   grid: 'адаптивная',
   features: 'галерея',
   price: '2100'
-},
-
-{
+  },
+  {
   src: './img/template-2.jpg',
   title: 'Vapid',
   desc: 'Учебный веб-ресурс',
   grid: 'резиновая',
   features: 'преимущества',
   price: '5600'
-},
-
-{
+  },
+  { 
   src: './img/template-3.jpg',
   title: 'We host',
   desc: 'Интернет-магазин модемов',
   grid: 'резиновая',
   features: 'корзина',
   price: '4700'
-},
-
- {
+  },
+  {
   src: './img/template-4.jpg',
   title: 'Elegant',
   desc: 'Посадочная бьюти страницы',
   grid: 'фиксированная',
   features: 'слайдер',
   price: '1200'
-},
-
-{
+  },
+  {
   src: './img/template-5.jpg',
   title: 'Brink',
   desc: 'Хоккейный новостной портал',
   grid: 'адаптивная',
   features: 'новости',
   price: '8300'
-},
-
- {
+  },
+  {
   src: './img/template-6.jpg',
   title: 'Roof',
   desc: 'Информационный сайт',
   grid: 'фиксированная',
   features: 'слайдер',
   price: '3300'
-}
+  }
 ]
