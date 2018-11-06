2b0130d49f8d69162a96503d84e554fe6ecb469d
safronman <safronmanbox@gmail.com>
2018-11-06 12:57:21 +0300

    add filter logic (temp file)

---
+++ reveb/js/filter-logic.js
@@ -0,0 +1,87 @@
+// let array = [2100, 5600, 4700, 1200, 8300, 3330];
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] > 1000 && array[i] < 5000) {
+//     console.log(array[i]);
+//   }
+// }
+
+// let resultArray = array.filter( (item) => {
+//   return item > 1000 && item < 5000;
+// });
+
+// console.log(resultArray);
+
+let array2 = [
+  {
+  src: './img/template-1.jpg',
+  title: 'Tesla car',
+  desc: 'Анимированный лэндинг',
+  grid: 'адаптивная',
+  features: 'галерея',
+  price: '2100'
+  },
+  {
+  src: './img/template-2.jpg',
+  title: 'Vapid',
+  desc: 'Учебный веб-ресурс',
+  grid: 'резиновая',
+  features: 'преимущества',
+  price: '5600'
+  },
+  { 
+  src: './img/template-3.jpg',
+  title: 'We host',
+  desc: 'Интернет-магазин модемов',
+  grid: 'резиновая',
+  features: 'корзина',
+  price: '4700'
+  },
+  {
+  src: './img/template-4.jpg',
+  title: 'Elegant',
+  desc: 'Посадочная бьюти страницы',
+  grid: 'фиксированная',
+  features: 'слайдер',
+  price: '1200'
+  },
+  {
+  src: './img/template-5.jpg',
+  title: 'Brink',
+  desc: 'Хоккейный новостной портал',
+  grid: 'адаптивная',
+  features: 'новости',
+  price: '8300'
+  },
+  {
+  src: './img/template-6.jpg',
+  title: 'Roof',
+  desc: 'Информационный сайт',
+  grid: 'фиксированная',
+  features: 'слайдер',
+  price: '3300'
+  }
+];
+
+// console.log(array2);
+
+// let resultArray2 = array2.filter(item => {
+//   return item.price > 1000 && item.price < 5000;
+// });
+
+// for (let i = 0; i < array2.length; i++) {
+//   if (array2[i].price > 1000 && array2[i].price < 5000) {
+//     console.log(array2[i].price);
+//   }
+// }
+
+// console.log(array2);
+
+// console.log(resultArray2);
+
+
+
+
+
+// this.templateArrayButton = document.querySelector('.button--del-padding');
+// this.templateArrayButton.addEventListener('click', this.showAllTemplateArray.bind(this)); 
